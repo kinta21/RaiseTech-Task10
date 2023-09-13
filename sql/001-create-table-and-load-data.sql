ee6c67f7dea5b480d027b1cdf87862352722db9f
kinta21 <kinta21127@yahoo.ne.jp>
2023-09-13 15:05:19 +0900

    lombok追加、キャメルケースに変更

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS musics;
 CREATE TABLE musics (
   id int unsigned AUTO_INCREMENT,
   composer VARCHAR(20) NOT NULL,
-  representative_song VARCHAR(20)NOT NULL,
+  representativeSong VARCHAR(20)NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO musics (composer, representative_song) VALUES ("ショパン", "幻想のポロネーズ");
-INSERT INTO musics (composer, representative_song) VALUES ("バッハ", "G線上のアリア");
-INSERT INTO musics (composer, representative_song) VALUES ("ベートーヴェン", "交響曲第７番");
+INSERT INTO musics (composer, representativeSong) VALUES ("ショパン", "幻想のポロネーズ");
+INSERT INTO musics (composer, representativeSong) VALUES ("バッハ", "G線上のアリア");
+INSERT INTO musics (composer, representativeSong) VALUES ("ベートーヴェン", "交響曲第７番");
