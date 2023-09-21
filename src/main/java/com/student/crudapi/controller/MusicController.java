f22fbf2e1ec92e4d97de180456fe8a25994ee533
kinta21 <kinta21127@yahoo.ne.jp>
2023-09-21 09:32:21 +0900

    build.gradleからjetbrains annotatios削除、Controllerの修正

--- src/main/java/com/student/crudapi/controller/MusicController.java
+++ src/main/java/com/student/crudapi/controller/MusicController.java
@@ -11,7 +11,14 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -31,7 +38,7 @@ public class MusicController {
 
     }
 
-   @ExceptionHandler(value = NotMusicFoundException.class)
+   @ExceptionHandler (value = NotMusicFoundException.class)
    public  ResponseEntity<Map<String,String>> handleNoResourceFound(
            NotMusicFoundException e, HttpServletRequest request){
         Map<String, String> body = Map.of(
@@ -42,18 +49,19 @@ public class MusicController {
                 "path", request.getRequestURI());
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
    }
-
-
+   
     @GetMapping("/musics")
     public List<Music> musics(){
         List<Music> musics = musicService.findAll();
         return musics;
     }
+
     @GetMapping("/musics/{id}")
     public MusicResponse getMusicById(@PathVariable("id") int id){
         Music music = musicService.findById(id);
         return new MusicResponse(music);
     }
+
     @PostMapping("/musics")
     public ResponseEntity<Music>createMusic(@RequestBody @Valid MusicCreateForm musicCreateForm, UriComponentsBuilder uriBuilder) {
         Music music = musicService.createMusic(musicCreateForm.getComposer(),musicCreateForm.getRepresentativeSong());
@@ -63,11 +71,13 @@ public class MusicController {
                 .toUri();
         return ResponseEntity.created(url).body(music);
     }
+
     @PatchMapping("/musics/{id}")
     public ResponseEntity<Map<String, String>> updateMusic(@PathVariable int id, @RequestBody MusicUpdateForm musicUpdateForm) throws Exception {
         musicService.updateMusic(id, musicUpdateForm.getComposer(), musicUpdateForm.getRepresentativeSong());
         return ResponseEntity.ok(Map.of("message", "music successfully updated"));
     }
+
     @DeleteMapping("/musics/{id}")
     public ResponseEntity<Map<String, String>> deleteById(@PathVariable int id) {
         musicService.deleteById(id);
